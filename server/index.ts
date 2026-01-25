a47deaf78cd509d83d54a6f91d7ba67ed3e1629c
Sean Linton <165918439+LBSconnect@users.noreply.github.com>
2026-01-25 15:18:49 -0600

    Add security middleware: helmet, rateLimit, cors

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,6 @@
-import express, { type Request, Response, NextFunction } from "express";
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";

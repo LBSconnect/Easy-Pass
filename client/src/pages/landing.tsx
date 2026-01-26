776e3e4d8190f4e545586720491e89c0768080d1
Sean Linton <165918439+LBSconnect@users.noreply.github.com>
2026-01-26 06:56:11 -0600

    Update landing.tsx

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -168,7 +168,7 @@ export default function LandingPage() {
                     Aprueba tu Examen de
                     <span className="text-primary"> Licencia de Texas </span>
                     <br className="block sm:hidden" />
-                    en el Primer Intento
+                     en el Primer Intento
                   </>
                 ) : (
                   <>
